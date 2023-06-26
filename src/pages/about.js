51d5eea99d94c4f1eaedb12525da99cfdc77ae22
KSSaiTeja <saitej4865@gmail.com>
2023-06-26 10:01:39 +0530

    (v14)

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,14 +3,12 @@ import Layout from "@/components/Layout";
 import Head from "next/head";
 import Image from "next/image";
 import React, { use, useEffect, useRef } from "react";
-import profilePic from "../../public/images/profile/developer-pic-2.jpg";
+import profilePic from "../../public/images/profile/test.jpg";
 import { useInView, useMotionValue, useSpring } from "framer-motion";
 import Skills from "@/components/Skills";
 import Experience from "@/components/Experience";
 import Education from "@/components/Education";
 import TransitionEffect from "@/components/TransitionEffect";
-// import Experience from "@/components/Experience";
-// import Education from "@/components/Education";
 
 const AnimatedNumbers = ({ value }) => {
   const ref = useRef(null);
@@ -56,20 +54,20 @@ const about = () => {
                 About Me
               </h2>
               <p className="font-medium">
-                Hi, I&apos;m K S Sai Teja, a Web Developer and UI/UX designer with a
-                passion for creating beautiful, functional, and user-centered
-                digital experiences. With 4 years of experience in the field. I
-                am always looking for new and innovative ways to bring my
-                client&apos;s visions to life. 
+                Hi, I&apos;m K S Sai Teja, a Web Developer and UI/UX designer
+                with a passion for creating beautiful, functional, and
+                user-centered digital experiences. With 4 years of experience in
+                the field. I am always looking for new and innovative ways to
+                bring my client&apos;s visions to life.
               </p>
               <p className="my-4 font-medium">
                 I believe that design is about more than just making things look
-                pretty – it&apos;s about solving problems and creating intuitive,
-                enjoyable experiences for users.{" "}
+                pretty – it&apos;s about solving problems and creating
+                intuitive, enjoyable experiences for users.{" "}
               </p>
               <p className="font-medium">
-                Whether I&apos;m working on a website, mobile app, or other digital
-                product, I bring my commitment to design excellence and
+                Whether I&apos;m working on a website, mobile app, or other
+                digital product, I bring my commitment to design excellence and
                 user-centered thinking to every project I work on. I look
                 forward to the opportunity to bring my skills and passion to
                 your next project.
