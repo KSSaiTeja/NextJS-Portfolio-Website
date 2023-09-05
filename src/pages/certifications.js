2fa137f040a89acc090684eb9e0e6b269fde4865
KSSaiTeja <saitej4865@gmail.com>
2023-09-05 23:36:08 +0530

    some bug fixes

--- src/pages/certifications.js
+++ src/pages/certifications.js
@@ -5,15 +5,18 @@ import { LinkedInIcon } from "@/components/Icons";
 const certifications = () => {
   return (
     <div className="flex flex-col justify-center items-center h-screen">
-      <h1 className="text-center font-bold">
+      <h1 className="text-center font-bold mb-4 ">
         My certifications will be uploaded soon..
         <hr />
       </h1>
 
-      <div style={display=inline-block}>
-        Meanwhile check <LinkedInIcon className="w-6 h-6" /> profile
+      <div class="inline-flex items-center">
+        Meanwhile check &nbsp;{" "}
+        <a href="https://linkedin.com/in/kssaiteja" target="_blank">
+          <LinkedInIcon class="w-6 h-6 cursor-pointer hover:scale-125" />
+        </a>{" "}
+        &nbsp; profile
       </div>
-
     </div>
   );
 };
