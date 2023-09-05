5e13f1d9fbecc5ec38d34cf70bdc1c17cf8f17c1
KSSaiTeja <saitej4865@gmail.com>
2023-09-05 23:40:07 +0530

    certifications sample page completed

--- src/pages/certifications.js
+++ src/pages/certifications.js
@@ -5,12 +5,12 @@ import { LinkedInIcon } from "@/components/Icons";
 const certifications = () => {
   return (
     <div className="flex flex-col justify-center items-center h-screen">
-      <h1 className="text-center font-bold mb-4 ">
+      <h1 className="text-center font-bold text-4xl">
         My certifications will be uploaded soon..
-        <hr />
+        <hr className="my-4"/>
       </h1>
 
-      <div class="inline-flex items-center">
+      <div class="inline-flex items-center text-3xl">
         Meanwhile check &nbsp;{" "}
         <a href="https://linkedin.com/in/kssaiteja" target="_blank">
           <LinkedInIcon class="w-6 h-6 cursor-pointer hover:scale-125" />
